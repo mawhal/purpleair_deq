5f04bfbee5dbd2639fdaa65173d7d44e2801dc7c
Jazlyn Howell <Jazlyn@192.168.1.7>
2023-07-20 16:44:26 -0400

    add read_excel

--- R/read_shirley.R
+++ R/read_shirley.R
@@ -1,8 +1,9 @@
 library(tidyverse)
 library(lubridate)
+library(readxl)
 
-#read exel sheet
-shirley = read_csv("/Users/Jazlyn/Downloads/shirley.csv")
+shirley <- read_excel("data/deq/Shirley PM minute report202307050601.XLS", sheet =1)
+
 
 
 #extract date
@@ -14,7 +15,7 @@ mdy(date)
 
 
 #extract data 
-shirley = read_csv("/Users/Jazlyn/Downloads/shirley.csv",skip = 11)
+shirley = read_excel("data/deq/Shirley PM minute report202307050601.XLS",skip = 11, sheet=1)
 shirley
 #select columns with data
 shirley=shirley[,c(1,4,7)]
@@ -32,17 +33,15 @@ tail(shirley)
 
 #combine date and time 
 
-shirley$time=paste(date,shirley$time)
-class(shirley$time)
-shirley$time = mdy_hm(shirley$time)
+# shirley$time=paste(date,shirley$time)
+# class(shirley$time)
+# shirley$time = mdy_hm(shirley$time)
 
 
 #plot time series 
 
-ggplot(shirley,aes(x=time,y=PM10))+geom_path
+ggplot(shirley,aes(x=time,y=PM10))+geom_path()
 
 ggplot(shirley,aes(x=time,y=PM25))+geom_path()
 
-library(readxl)
 
-shirley <- read_excel("Shirley PM minute report202307050601.xls", sheet =1)
