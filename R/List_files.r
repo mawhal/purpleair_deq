d324eef407c99836fc2866bd7b8474b068ccadcf
Matthew A Whalen <mwhalen@vsu.edu>
2023-07-28 15:21:27 -0400

    finish deq merge script

--- R/List_files.r
+++ R/List_files.r
@@ -1,19 +1,30 @@
 # vector of file names
-files <- list.files(path = "data/deq/", pattern = ".XLS")
+files <- list.files(path = "data/deq/", pattern = ".XLS", full.names = T)
 files
 
 
-
+# load packages
 library(tidyverse)
 library(lubridate)
+library(hms)
 library(readxl)
 
-
+# read the function from another R script
 source("R/read_shirley.r")
-read_shirley(files[[1]])
+# test read
+# read_shirley(files[[1]])
 
-str(files)
-files
+# read multiple files at once
 shirley = lapply(files,read_shirley)
 str(shirley)
 lapply(shirley, head)
+
+
+# let's combine all list elements into a single data.frame
+shirley.long = do.call( rbind, shirley )
+
+
+## write to disk
+write_csv(shirley.long, "output/deq_data_merge.csv")
+
+
--- R/read_shirley.R
+++ R/read_shirley.R
@@ -1,33 +1,31 @@
 read_shirley = function(data){
   
-  
-  
+# test data
+# data = "data/deq/Shirley PM minute report202307050601.XLS"
 
-
-
-  shirley <- read_excel("data/deq/Shirley PM minute report202307050601.XLS", sheet =1)
+# grab date information from the file
+shirley <- read_excel(data, sheet =1)
   #extract date
-  class(shirley)
-  dim(shirley)
   shirley[3,12]
-  date = shirley[3,12]
-  mdy(date)
+  date = mdy( shirley[3,12] )
 
 #extract data 
-shirley = read_excel(data,skip = 11, sheet=1)
-shirley
+shirley = read_excel(data, skip = 11, sheet = 1)
 #select columns with data
-shirley=shirley[,c(1,4,7)]
+shirley = shirley[,c(1,4,7)]
 
 
-#rename columns 
-
+# rename columns 
 colnames(shirley) = c("time","PM10","PM25")
 
-#delete statistic rows 
-
+# delete statistic rows 
 shirley = shirley[1:1440,]
 # tail(shirley)
+
+# replace date information
+shirley$time = as_hms(shirley$time)
+# paste the date
+shirley$time_stamp <- ymd_hms( paste( date, shirley$time ) )
 
 return(shirley)
 }
