3e5eb6b34cd0e97edc89976afa44515e12900733
Jen Adu <Jadu0192@students.vsu.edu>
2023-07-31 15:12:24 -0400

    Add pm25 analysis

--- R/compare_deq_purpleair.R
+++ R/compare_deq_purpleair.R
@@ -117,3 +117,84 @@ dev.off()
 
 # detemine which dates are represented
 unique(date(dcomplete$time_stamp))
+
+
+
+# run the analysis for the PM2.5 data
+# take the mean across all sensors and use this for comparison to DEQ
+purp.mean <- purp %>% 
+  group_by(time_stamp) %>% 
+  summarize( pm25purp = mean(pm25purp))
+
+
+## issue 1
+# DEQ data recorded every minute, while PurpleAir recorded every ten minutes
+# solution?
+#   - 1) reduce DEQ dataset to only show data collected on the hour and in ten-minute increments
+#   - 2) average DEQ data every ten minutes
+#   
+
+# option 1
+# let's try to merge the dataset based on time_stamp
+dcombine <- left_join( deq, purp.mean )
+# set x and y limits
+# remove NA
+dcomplete <- dcombine[ complete.cases(dcombine), ]
+dcomplete$pm25deqlog <- log(dcomplete$pm25deq)
+dcomplete$pm25purplog <- log(dcomplete$pm25purp)
+# correlation test
+cor.deq.purple <- cor.test( dcomplete$pm25deq, dcomplete$pm25purp )
+cor.deq.purple$estimate
+cor.deq.purple$conf.int
+lm.deq.purple <- lm(pm25purp ~ pm25deq, data = dcomplete)
+xylim <- range( c(dcomplete$pm25deq, dcomplete$pm25purp), na.rm = T )
+svg("output/correlation_deq_purplemean.svg", width = 3, height = 3)
+par(mar = c(5,4,2,2)+0.1, pty = "s", las = 1  )
+plot(dcomplete$pm25deq, dcomplete$pm25purp, 
+     cex = 0.5, xlim = c(0,xylim[2]), ylim = c(0,xylim[2]),
+     xlab = expression(paste("pm25 DEQ (", mu, "g/",m^3,")")),
+     ylab = expression(paste("pm25 PurpleAir (", mu, "g/",m^3,")")),
+     type = "n" )
+points( dcomplete$pm25deq, dcomplete$pm25purp, col = "slateblue")
+abline(a = 0, b = 1, col = 'red')
+abline(lm.deq.purple)
+text( x = 50, y = 140,
+      labels = paste0("r = ", round(cor.deq.purple$estimate,3)), 
+      adj = 0, cex = 0.75 )
+text(125,125,labels = "1:1", adj = 0.5, pos = 3, 
+     cex = 0.75, srt = 45, col = "red" )
+dev.off()
+# slope is the Estimate for pm25deq
+summary(lm.deq.purple)$coefficients # slope is ~1.3 and is highly significant
+# 
+
+####
+# Log-log transform
+lm.deq.purple.log <- lm(pm25purplog ~ pm25deqlog, data = dcomplete)
+xylim <- range( c(dcomplete$pm25deqlog, dcomplete$pm25purplog), na.rm = T )
+svg("output/correlation_deq_purplemean_log.svg", width = 3, height = 3)
+par(mar = c(5,4,2,2)+0.1, pty = "s", las = 1  )
+plot(dcomplete$pm25deqlog, dcomplete$pm25purplog, 
+     cex = 0.5, xlim = xylim, ylim = xylim,
+     xlab = expression(paste("ln(pm25 DEQ (", mu, "g/",m^3,"))")),
+     ylab = expression(paste("ln(pm25 PurpleAir (", mu, "g/",m^3,"))")),
+     type = "n" )
+points( dcomplete$pm25deqlog, dcomplete$pm25purplog, col = "slateblue")
+abline(a = 0, b = 1, col = 'red')
+abline(lm.deq.purple.log)
+text( x = 50, y = 140,
+      labels = paste0("r = ", round(cor.deq.purple$estimate,3)), 
+      adj = 0, cex = 0.75 )
+text(125,125,labels = "1:1", adj = 0.5, pos = 3, 
+     cex = 0.75, srt = 45, col = "red" )
+dev.off()
+
+# plot time series together
+svg("output/time_series_deq_purplemean.svg", width = 6, height = 3)
+par(mar = c(5,5,2,2)+0.1, pty = "m", las = 1  )
+plot( y = dcomplete$pm25deq, x = dcomplete$time_stamp, type = "n", 
+      xlab = "Time",
+      ylab = expression(paste("pm25 (", mu, "g/",m^3,")"))  )
+points(y = dcomplete$pm25purp, x = dcomplete$time_stamp, pch = 16, cex = 0.5,  col = "purple" )
+lines( x = dcomplete$time_stamp, y = dcomplete$pm25deq )
+dev.off()
